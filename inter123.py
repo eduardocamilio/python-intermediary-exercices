8864ceff34cf6803165c4677a4f2c5f56a0278c1
eduardocamilio <eduardocamilio137@gmail.com>
2023-05-17 22:14:48 -0300

    Learning

---
+++ inter123.py
@@ -0,0 +1,22 @@
+p1 = {
+    'nome': 'Eduardo',
+    'sobrenome': 'Camilio',
+}
+# print(p1['nome'])
+# print(p1.get('nome', 'Não existe'))
+
+# nome = p1.pop('nome')
+# print(nome)
+# print(p1)
+# ultima_chave = p1.popitem()
+# print(ultima_chave)
+# print(p1)
+# p1.update({
+#     'nome': 'novo valor',
+#     'idade': 30,
+# })
+# p1.update(nome='novo valor', idade=30)
+# tupla = (('nome', 'novo valor'), ('idade', 30))
+lista = [['nome', 'novo valor'], ['idade', 30]]
+p1.update(lista)
+print(p1)
