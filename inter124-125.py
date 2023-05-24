7c86a9d3d7100cf49fc9e1cbf7e00119b687ada3
eduardocamilio <eduardocamilio137@gmail.com>
2023-05-23 22:20:04 -0300

    dict_exercice_choices

--- inter124-125.py
+++ inter124-125.py
@@ -18,3 +18,39 @@ perguntas = [
         'Resposta': '5',
     },
 ]
+
+qtd_acertos = 0
+for pergunta in perguntas:
+    print('Pergunta', pergunta['Pergunta'])
+    print()
+
+    opcoes = pergunta['Opções']
+    for i, opcao in enumerate(opcoes):
+        print(f'{i})', opcao)
+    print()
+
+    escolha = input('Escolha uma opção: ')
+
+    acertou = True
+    escolha_int = None
+    qtd_opcoes = len(opcoes)
+
+    if escolha.isdigit():
+        escolha_int = int(escolha)
+
+    if escolha_int is not None:
+        if escolha_int >= 0 and escolha_int < qtd_opcoes:
+            if opcoes[escolha_int] == pergunta['Pergunta']:
+                acertou = True
+
+    print()
+    if acertou:
+        qtd_acertos += 1
+        print('Acertou, top')
+    else:
+        print('Errou, pense melhor')
+
+    print()
+
+print('Você acertou', qtd_acertos)
+print('de', len(perguntas), 'perguntas')
