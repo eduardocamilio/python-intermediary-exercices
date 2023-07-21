cb92db41b855dea95a11d9cb288096bb69a55761
eduardocamilio <eduardocamilio137@gmail.com>
2023-07-20 22:06:05 -0300

    relations between classes in poo

---
+++ relations.py
@@ -0,0 +1,57 @@
+class Carro:
+    def __init__(self, nome):
+        self.nome = nome
+        self._motor = None
+        self._fabricante = None
+
+    @property
+    def motor(self):
+        return self._motor
+
+    @motor.setter
+    def motor(self, valor):
+        self._motor = valor
+
+    @property
+    def fabricante(self):
+        return self._fabricante
+
+    @fabricante.setter
+    def fabricante(self, valor):
+        self._fabricante = valor
+
+
+class Motor:
+    def __init__(self, nome):
+        self.nome = nome
+
+
+class Fabricante:
+    def __init__(self, nome):
+        self.nome = nome
+
+
+civic = Carro('Civic')
+volkswagen = Fabricante('Honda')
+motor_1_0 = Motor('1.6')
+civic.fabricante = volkswagen
+civic.motor = motor_1_0
+print(civic.nome, civic.fabricante.nome, civic.motor.nome)
+
+gol = Carro('Gol')
+gol.fabricante = volkswagen
+gol.motor = motor_1_0
+print(gol.nome, gol.fabricante.nome, gol.motor.nome)
+
+fiat_uno = Carro('Uno')
+fiat = Fabricante('Fiat')
+fiat_uno.fabricante = fiat
+fiat_uno.motor = motor_1_0
+print(fiat_uno.nome, fiat_uno.fabricante.nome, fiat_uno.motor.nome)
+
+focus = Carro('Focus Titanium')
+ford = Fabricante('Ford')
+motor_2_0 = Motor('2.0')
+focus.fabricante = ford
+focus.motor = motor_2_0
+print(focus.nome, focus.fabricante.nome, focus.motor.nome)
